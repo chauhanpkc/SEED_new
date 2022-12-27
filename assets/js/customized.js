7544f66c143b74908a6fda55d38747ef59070ea7
chauhanpkc <chauhanprashant@ymail.com>
2022-12-27 17:41:36 +0530

    update

--- assets/js/customized.js
+++ assets/js/customized.js
@@ -1,13 +1,41 @@
 $(document).ready(function () {
-    $(".email").change(function () {    
+   // ========Email validation===========
+    $(".email").on('change keyup',function () {    
         var inputvalues = $(this).val();    
-        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;    
+        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
         if(!regex.test(inputvalues)){    
-            $(".email_msg").text("Invalid E-mail ID");
-            $(".email_msg").color("Invalid E-mail ID");    
+            $(".email_msg").text("Invalid E-mail ID"); 
         return regex.test(inputvalues);    
         }    
         }); 
+      //   =======Mobile Number Validation=========
+      $(".m_number").on('change keyup',function () {    
+         var mobilevalues = $(this).val();    
+         var regex = /^[6-9]{1}[0-9]{9}$/; 
+         if(!regex.test(mobilevalues)){    
+             $(".m_number_msg").text("Invalid Mobile number"); 
+         return regex.test(mobilevalues);    
+         }    
+         });
+      //   =======Aadhar card validation========
+      $(".aadhar").on('change keyup',function () {    
+         var aadharvalues = $(this).val();    
+         var regex = /^[2-9]{1}[0-9]{3}\s[0-9]{4}\s[0-9]{4}$/; 
+         if(!regex.test(aadharvalues)){    
+             $(".aadhar_msg").text("Invalid Aadhar Card number"); 
+         return regex.test(aadharvalues);    
+         }    
+         console.log('');
+         });
+         // ==========IFSC Code Validation======
+         $(".ifsc").on('change keyup',function () {    
+            var ifscvalues = $(this).val();    
+            var regex = /^[A-Z]{4}[0-9]{7}$/; 
+            if(!regex.test(ifscvalues)){    
+                $(".ifsc_msg").text("Invalid IFSC Code"); 
+            return regex.test(ifscvalues);    
+            }    
+            });
     var minLength = 1;
     var maxLength = 50;
     $(".min-max-name").on("keydown keyup change", function () {
@@ -30,4 +58,16 @@ $(document).ready(function () {
        else
           $(".min-max-fname-message").text("");
     });
+    var minLength = 1;
+    var maxLength = 50;
+    $(".min-max-occupation").on("keydown keyup change", function () {
+       var value = $(this).val();
+       if (value.length < minLength)
+          $(".min-max-occupation-message").text("Minimum " + minLength + " character required.");
+       else if (value.length > maxLength)
+          $(".min-max-occupation-message").text("Maximum " + maxLength + " character allowed.");
+       else
+          $(".min-max-occupation-message").text("");
+    });
+    
 });    
--- assets/js/validator.js
+++ assets/js/validator.js
@@ -1,34 +1,98 @@
-$("#register").click(function () {
-    var reg_email = $("#email");
-    if (reg_email.val() === "") {
-        document.getElementById("email_message").innerHTML = "Email can not be empty";
-        document.getElementById("email_message").style.display = "block";
+// $("#register").click(function () {
+//     var reg_email = $("#email");
+//     if (reg_email.val() === "") {
+//         document.getElementById("email_message").innerHTML = "Email can not be empty";
+//         document.getElementById("email_message").style.display = "block";
+//     }
+//     else {
+//         document.getElementById("email_message").innerHTML = "";
+//     }
+//     var reg_name = $("#name");
+//     if (reg_name.val() === "") {
+//         document.getElementById("name_message").innerHTML = "Name can not be empty";
+//         document.getElementById("name_message").style.display = "block";
+//     }
+//     else {
+//         document.getElementById("name_message").innerHTML = "";
+//     }
+//     var reg_fname = $("#fname");
+//     if (reg_fname.val() === "") {
+//         document.getElementById("fname_message").innerHTML = "Father Name can not be empty";
+//         document.getElementById("fname_message").style.display = "block";
+//     }
+//     else {
+//         document.getElementById("fname_message").innerHTML = "";
+//     }
+//     var reg_passport = $("#passport");
+//     if (reg_passport.val() === "") {
+//         document.getElementById("passport_message").innerHTML = "Passport photo can not be empty";
+//         document.getElementById("passport_message").style.display = "block";
+//     }
+//     else {
+//         document.getElementById("passport_message").innerHTML = "";
+//     }
+// });
+// $(function() {   
+//     var minLength = 10;
+//     var maxLength = 10;
+//     $("#m_number").on("keydown keyup change", function(){
+//       var value = $(this).val();
+//       if (value.length < minLength)
+//           $(".m_numbr_msg").text("Please enter Valid Mobile Number.");
+//       else if (value.length > maxLength)
+//           $(".m_numbr_msg").text("Enter 10 Digit Mobile Number");
+//       else
+//           $(".m_numbr_msg").text("");
+//     });
+//   });
+//   =======Passport Size Photo=========
+  var passport_s = "";
+    $('#passport_size').on('change', function () {
+    passport_s = $("#passport_size").val();
+    console.log(passport_s);
+    var upld = passport_s.split('.').pop();
+    if (upld == 'jpg') {
+        // alert("File uploaded is pdf")
+     } else {
+        alert("Only .Jpg Image are allowed")
+        }
+    if (this.files[0].size > 204800) {
+        alert("Please upload .jpg Image file less than 200KB ....");
+        $(this).val('');
     }
-    else {
-        document.getElementById("email_message").innerHTML = "";
+
+});
+// =============Cast Certificate========
+  var myFile = "";
+    $('#cast_cer').on('change', function () {
+    myFile = $("#cast_cer").val();
+    console.log(myFile);
+    var upld = myFile.split('.').pop();
+    if (upld == 'pdf') {
+        // alert("File uploaded is pdf")
+     } else {
+        alert("Only PDF are allowed")
+        }
+    if (this.files[0].size > 204800) {
+        alert("Please upload pdf file less than 200KB ....");
+        $(this).val('');
     }
-    var reg_name = $("#name");
-    if (reg_name.val() === "") {
-        document.getElementById("name_message").innerHTML = "Name can not be empty";
-        document.getElementById("name_message").style.display = "block";
+
+});
+// ================Bank first page/ Cheque Validation==========
+var bank_passbook = "";
+    $('#bank_first').on('change', function () {
+    bank_passbook = $("#bank_first").val();
+    console.log(bank_passbook);
+    var upld = bank_passbook.split('.').pop();
+    if (upld == 'pdf') {
+        // alert("File uploaded is pdf")
+     } else {
+        alert("Only PDF are allowed")
+        }
+    if (this.files[0].size > 204800) {
+        alert("Please upload pdf file less than 200KB ....");
+        $(this).val('');
     }
-    else {
-        document.getElementById("name_message").innerHTML = "";
-    }
-    var reg_fname = $("#fname");
-    if (reg_fname.val() === "") {
-        document.getElementById("fname_message").innerHTML = "Father Name can not be empty";
-        document.getElementById("fname_message").style.display = "block";
-    }
-    else {
-        document.getElementById("fname_message").innerHTML = "";
-    }
-    var reg_passport = $("#passport");
-    if (reg_passport.val() === "") {
-        document.getElementById("passport_message").innerHTML = "Passport photo can not be empty";
-        document.getElementById("passport_message").style.display = "block";
-    }
-    else {
-        document.getElementById("passport_message").innerHTML = "";
-    }
+
 });
