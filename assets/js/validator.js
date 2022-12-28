a8de32d9aa635119a5e346f09483d8bbcd7b076e
chauhanpkc <chauhanprashant@ymail.com>
2022-12-28 10:11:58 +0530

    change

--- assets/js/validator.js
+++ assets/js/validator.js
@@ -45,6 +45,82 @@
 //           $(".m_numbr_msg").text("");
 //     });
 //   });
+
+$(document).ready(function () {
+    // ========Email validation===========
+     $(".email").on('change keyup',function () {    
+         var inputvalues = $(this).val();    
+         var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+         if(!regex.test(inputvalues))    
+             $(".email_msg").text("Invalid E-mail ID"); 
+         else $(".email_msg").text("");  
+             
+         }); 
+       //   =======Mobile Number Validation=========
+       $(".m_number").on('change keyup',function () {    
+          var mobilevalues = $(this).val();    
+          var regex = /^[6-9]{1}[0-9]{9}$/; 
+          if(!regex.test(mobilevalues))    
+              $(".m_number_msg").text("Invalid Mobile number"); 
+          else
+          $(".m_number_msg").text("");
+          });
+       //   =======Aadhar card validation========
+       $(".aadhar").on('change keyup',function () {    
+          var aadharvalues = $(this).val();    
+          var regex = /^[2-9]{1}[0-9]{3}\s[0-9]{4}\s[0-9]{4}$/; 
+          if(!regex.test(aadharvalues))    
+              $(".aadhar_msg").text("Invalid Aadhar Card number"); 
+          else
+          $(".aadhar_msg").text("");    
+              
+          console.log('');
+          });
+          // ==========IFSC Code Validation======
+          $(".ifsc").on('change keyup',function () {    
+             var ifscvalues = $(this).val();    
+             var regex = /^[A-Z]{4}[0-9]{7}$/; 
+             if(!regex.test(ifscvalues))   
+                 $(".ifsc_msg").text("Invalid IFSC Code"); 
+             else 
+             $(".ifsc_msg").text("");   
+                 
+             });
+     var minLength = 1;
+     var maxLength = 50;
+     $(".min-max-name").on("keydown keyup change", function () {
+        var value = $(this).val();
+        if (value.length < minLength)
+           $(".min-max-name-message").text("Minimum " + minLength + " character required.");
+        else if (value.length > maxLength)
+           $(".min-max-name-message").text("Maximum " + maxLength + " character allowed.");
+        else
+           $(".min-max-name-message").text("");
+     });
+     var minLength = 1;
+     var maxLength = 50;
+     $(".min-max-fname").on("keydown keyup change", function () {
+        var value = $(this).val();
+        if (value.length < minLength)
+           $(".min-max-fname-message").text("Minimum " + minLength + " character required.");
+        else if (value.length > maxLength)
+           $(".min-max-fname-message").text("Maximum " + maxLength + " character allowed.");
+        else
+           $(".min-max-fname-message").text("");
+     });
+     var minLength = 1;
+     var maxLength = 50;
+     $(".min-max-occupation").on("keydown keyup change", function () {
+        var value = $(this).val();
+        if (value.length < minLength)
+           $(".min-max-occupation-message").text("Minimum " + minLength + " character required.");
+        else if (value.length > maxLength)
+           $(".min-max-occupation-message").text("Maximum " + maxLength + " character allowed.");
+        else
+           $(".min-max-occupation-message").text("");
+     });
+     
+ });    
 //   =======Passport Size Photo=========
   var passport_s = "";
     $('#passport_size').on('change', function () {
